1ce14e243d37b781347e5f273f9ea8afaa617069
mjowers <melissa.anne.jowers@gmail.com>
2015-03-28 18:12:56 -0400

    Refactor API class and add first test for it
    The fact that I’m doing the same parsing logic in the test is probably
    a bad sign, but I’ll deal with it later.

--- lib/farmers_market_api.rb
+++ lib/farmers_market_api.rb
@@ -1,10 +1,20 @@
 require 'net/http'
 require 'json'
 
+# Service class to communicate with the API and convert responses to ruby
+# objects
 class FarmersMarketAPI
+
+  # External call to the API
+  # todo: error handling 
+  def call_api(path)
+    response = Net::HTTP.get("search.ams.usda.gov", "/farmersmarkets/v1/data.svc#{path}")
+    JSON.parse(response)['results']
+  end
+
+  # Returns an array of Market objects for the given zipcode
   def get_markets(zipcode)
-    response = Net::HTTP.get("search.ams.usda.gov", "/farmersmarkets/v1/data.svc/zipSearch?zip=#{zipcode}")
-    results = JSON.parse(response)['results']
+    results = call_api("/zipSearch?zip=#{zipcode}")
     markets = results.map do |result|
       Market.new result['id'], result['marketname']
     end
